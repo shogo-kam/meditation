6450c6339f82328eeb8a85cee22c64700cba6b02
Toshiki Tsuchida <toshiki.tsuchida.dev@gmail.com>
2020-09-19 15:44:19 +0900

    音楽を変更したら、再生する音楽も変えるようにした。

--- src/components/Root.js
+++ src/components/Root.js
@@ -11,6 +11,14 @@ const TIMER_MODE = {
 	START: 'start',
 };
 
+const MUSIC_LIST = {
+	ETHNIC_01: 'ethnic_01',
+	HEALING_01: 'healing_01',
+	HEALING_04: 'healing_04',
+	HEALING_05: 'healing_05',
+	HEALING_06: 'healing_06',
+}
+
 class Root extends React.Component {
 	constructor(props) {
 		super(props);
@@ -23,6 +31,7 @@ class Root extends React.Component {
 		this.state = {
 			now: DEF_T,
 			timerMode: TIMER_MODE.STOP,
+			selectedMusic: MUSIC_LIST.ETHNIC_01,
 		};
 		this.audioResources = new AudioResources();
 		this.music = new Audio(this.audioResources.getSelectedMusic());
@@ -91,6 +100,8 @@ class Root extends React.Component {
 		this.setState({
 			timerMode: TIMER_MODE.STOP
 		});
+		// mock: 曲選択機能ができるまでのモック。一度停止すると曲を変更する。
+		this.musicChange();
 	}
 
 	tick() {
@@ -100,6 +111,25 @@ class Root extends React.Component {
 		this.setState({
 			now: this.state.now -=1
 		});
+	}
+
+	musicChange() {
+		this.setState({
+			selectedMusic: MUSIC_LIST.HEALING_01,
+		});
+	}
+
+	shouldComponentUpdate(nextProps, nextState) {
+		if(this.state != nextState) {
+
+			// 選択された音声が変わったら、再生する音声も変える
+			if(this.state.selectedMusic != nextState.selectedMusic) {
+				this.audioResources.selectMusic(nextState.selectedMusic);
+				this.music.src = this.audioResources.getSelectedMusic();
+			}
+			return true;
+		}
+		return false;
 	}
 
 	render() {
